751ed193673df978793f47f11ea3d9bc74833ea0
Donizetti Aparecido de Souza Junior <71847472+souza10v@users.noreply.github.com>
2023-04-29 18:43:11 -0300

    - Load data file and style to the project - Created the json reader and style reader - Initalizating the file to get the files when run the app
    (- add assets
    - add data files
    - parso son in content model
    - pasre style data in content model)

--- Learning/ViewModels/ContentModel.swift
+++ Learning/ViewModels/ContentModel.swift
@@ -9,4 +9,55 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    
+    var styleData: Data?
+    
+    init() {
+        
+        getLocalData()
+        
+    }
+    
+    func getLocalData() {
+        
+        // Get a url to the json file
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+            // Read the file into a data object
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            
+            let jsonDecoder = JSONDecoder()
+            
+            try modules = jsonDecoder.decode([Module].self, from: jsonData)
+            
+            // Assign parsed modules to modules property
+            self.modules = modules
+            
+        }
+        catch {
+            // Todo log error
+            print("Couln`t parse local data")
+        }
+        
+        // Parse the style data
+        
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+            
+            //Read the file into a data object
+            let styleData = try Data(contentsOf: styleUrl!)
+            
+            self.styleData = styleData
+            
+        }
+        
+        catch {
+            
+            print("Couln`t parse style data")
+            
+        }
+    }
 }
--- Learning/Views/HomeView.swift
+++ Learning/Views/HomeView.swift
@@ -13,9 +13,6 @@ struct HomeView: View {
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("Hello, world!")
         }
         .padding()
