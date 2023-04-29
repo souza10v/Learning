797e218af9a4cf065364c68ad2a7c82ce326e0c4
Donizetti Aparecido de Souza Junior <71847472+souza10v@users.noreply.github.com>
2023-04-29 13:24:47 -0300

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Donizetti de Souza on 4/29/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
