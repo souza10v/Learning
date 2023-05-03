a07845b1876d1d0d960fd6812b2f2c2a64f52e7e
Donizetti Aparecido de Souza Junior <71847472+souza10v@users.noreply.github.com>
2023-05-03 16:51:27 -0300

    -put answers with buttons -submit button in testview - highlight appropiate colors

--- Learning/Views/TestView.swift
+++ Learning/Views/TestView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct TestView: View {
     
     @EnvironmentObject var model: ContentModel
+    @State var selectedAnswerIndex: Int?
+    @State var submitted = false
+    @State var numCorrect = 0
     
     var body: some View {
         
@@ -24,10 +27,92 @@ struct TestView: View {
                     
                     // Question
                     CodeTextView()
+                        .padding(.horizontal, 20)
                     
                     // Answer
+                    ScrollView {
+                        VStack {
+                            ForEach(0..<model.currentQuestion!.answers.count, id: \.self) { index in
+                                
+                                Button {
+                                    // Track selected index
+                                    selectedAnswerIndex = index
+                                    
+                                } label: {
+                                    
+                                    ZStack {
+                                        
+                                        if submitted == false {
+                                            RectangleCard(color: index == selectedAnswerIndex ? .gray : .white)
+                                            
+                                        } else {
+                                            // Answer has been submitted
+                                            if index == model.currentQuestion!.correctIndex && index == selectedAnswerIndex {
+                                                
+                                                // User has selected right answer
+                                                // Show a green background
+                                                RectangleCard(color: Color.green)
+                                                    .frame(height: 48)
+                                            }
+                                            else if index == selectedAnswerIndex &&
+                                                        index != model.currentQuestion!.correctIndex {
+                                                
+                                                // User has selected wrong answer
+                                                // Show a red background
+                                                RectangleCard(color: Color.red)
+                                                    .frame(height: 48)
+                                            }
+                                            else if index == model.currentQuestion! . correctIndex {
+                                                
+                                                // This button is the corret answer
+                                                // Show a green background
+                                                RectangleCard(color: Color.green)
+                                                    .frame(height: 48)
+                                            }
+                                            else {
+                                                RectangleCard(color: Color.white)
+                                                    .frame(height: 48)
+                                            }
+                                        }
+                                        
+                                        Text(model.currentQuestion!.answers[index])
+        
+                                    }
+                                }
+                                .disabled(submitted)
+                            }
+                        }
+                        .accentColor(.black)
+                        .padding()
+                    }
                     
                     // Button
+                    Button {
+                        
+                        // Check the answer and incremente the counter if corret
+                        submitted = true
+                        
+                        if selectedAnswerIndex == model.currentQuestion!.correctIndex {
+                            numCorrect += 1
+                            
+                        }
+                        
+                    } label : {
+                        
+                        ZStack {
+                            
+                            RectangleCard(color: .green)
+                                .frame(height: 48)
+                            
+                            Text("Submit")
+                                .foregroundColor(Color.white)
+                                .bold()
+                            
+                        }
+                        .padding()
+                    }
+                    .disabled(selectedAnswerIndex == nil)
+                    
                 }
                 .navigationBarTitle("\(model.currentModule?.category ?? "") Test")
             }
